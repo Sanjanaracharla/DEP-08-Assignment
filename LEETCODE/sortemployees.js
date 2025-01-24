d85f6baff5c835b1de7b12cff7bfc487a5d3d447
Sanjanaracharla <racharla_sanjana@epam.com>
2025-01-24 12:06:58 +0530

    Create sortemployees.js

---
+++ LEETCODE/sortemployees.js
@@ -0,0 +1,7 @@
+let employees = [
+    { name: 'John', dob: 'Dec 15, 2007', score: 80 },
+    { name: 'Ana', dob: 'Jan 15, 2009', score: 75 },
+    { name: 'Zion', dob: 'Feb 15, 2011', score: 90 },
+  ];
+  employees.sort((a,b)=>a.score-b.score);
+  console.log(employees)
